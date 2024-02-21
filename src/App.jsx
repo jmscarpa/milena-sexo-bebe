2625d1002624c41c6eef973ad144eb4d53ecb1e1
João Mateus Scarpa <joao.scarpa@andela.com>
2024-02-21 20:49:27 -0300

    Changing text

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div className="background fade-in">
         <img src={girl} alt="Ícone de um bebê" className="icon-answer" />
         <h1 className="answer custom-font">É uma menina!</h1>
-        <p className="message custom-font">Mamãe e Papai estão muito felizes com a minha chegada!</p>
+        <p className="message custom-font">Mamãe, papai e meus irmãos estão muito felizes com a minha chegada!</p>
         <a href="https://wa.me/5512996517183" target="_blank" rel="noreferrer">
           <button className="whatsapp">Mande uma mensagem para mim</button>
         </a>
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="background">
       <img src={icon} alt="Ícone de um bebê" width={256}/>
-      <h1 className="custom-font">A família está crescendo e a gente quer saber...</h1>
+      <h1 className="custom-font">A família cresceu mais um pouquinho...</h1>
       <div className="buttons">
         <button className="its-a-boy" onClick={() => setShowAnswer(true)}>É um menino</button>
         <button className="its-a-girl" onClick={() => setShowAnswer(true)}>É uma menina</button>
